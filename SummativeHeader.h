96df678741ec21799a804d3e2cab6cd68d14b1a2
LoveUtoo <45604857+LoveUtoo@users.noreply.github.com>
2018-12-04 12:59:27 -0500

    Create SummativeHeader.h

---
+++ SummativeHeader.h
@@ -0,0 +1,12 @@
+const int SCREEN_W = 900;       // screen width
+const int SCREEN_H = 480;       // screen height
+
+//Define structs
+stuct wordbank {
+    //characters you have typed so far
+    char hotbar [20];
+    //words on screen
+    char astWords [20][20];
+};
+
+
