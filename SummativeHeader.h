31365025fc48c05ab4ffc9be0418ef5883760737
LoveUtoo <45604857+LoveUtoo@users.noreply.github.com>
2018-12-04 13:26:40 -0500

    Update SummativeHeader.h

--- SummativeHeader.h
+++ SummativeHeader.h
@@ -2,11 +2,13 @@ const int SCREEN_W = 900;       // screen width
 const int SCREEN_H = 480;       // screen height
 
 //Define structs
-stuct wordbank {
+stuct WordBank {
     //characters you have typed so far
     char hotbar [20];
     //words on screen
     char astWords [20][20];
 };
 
+void checkDeath (int a);
+void printDeathscreen (int a);
 
