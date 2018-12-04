fd25b6e13afd15c8732d6043a0f4db1a29bc7de6
LoveUtoo <45604857+LoveUtoo@users.noreply.github.com>
2018-12-04 13:37:24 -0500

    Update SummativeMain.cpp

--- SummativeMain.cpp
+++ SummativeMain.cpp
@@ -1,1 +1,19 @@
-
+/*****************************************************************************
+ *	Name:     Nicolas                                                        *
+ *	Date:     November 2018                                                  *
+ *                                                                           *
+ *	Purpose: Space-Word Blaster                                              *
+ *	                                                                         *
+ *	Usage:   Allow us to show how we can use allegro to make games           *
+ *	                                                                         *
+ *	Revision History:                                                        *
+ *	                                                                         *
+ *	Known Issues:                                                            *
+ *	                                                                         *
+ *****************************************************************************/
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>                   // For allegro, must be in compiler search path.
+#include <allegro5/allegro_native_dialog.h> 		// for message box
+#include "SummativeHeader"
