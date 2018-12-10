ec0de20c5048a3b58278224b45c104513c72351e
LoveUtoo <45604857+LoveUtoo@users.noreply.github.com>
2018-12-10 13:25:01 -0500

    Update SummativeFunctions.cpp

--- SummativeFunctions.cpp
+++ SummativeFunctions.cpp
@@ -5,33 +5,48 @@
 #include <allegro5/allegro_native_dialog.h> 		// for message box
 #include "SummativeHeader.h"
 
-void printTitleScreen() {
-    WordBank game;
-    Stats player;
-  
-  
+char determineDifficulty() {
+    
+}
+
+void printTitleScreen(ALLEGRO_FONT *font) {
+    al_draw_text(font, TEXTCOLOR, 200, 5, ALLEGRO_ALIGN_CENTRE, "Game");
+    
+    al_flip_display();
 }
 
 void startGame() {
-    char difficulty;
+    int counter = 0;
     FILE *fptr;
     printf("What difficulty do you want to play? Easy, Medium, Hard (e/m/h)\n");
-    scanf("%c", difficulty);
-    while (true){
-    if (difficulty == 'e'){
+    char difficulty = determineDifficulty();
+    if (difficulty = 'e'){
         fptr = fopen("WordBankEasy.txt", "r");
-        break;
-    }else if (difficulty == 'm'){
+    
+        while (fscanf(fptr, "%s", game[counter].OffscreenWords) != EOF){
+            fscanf(fptr, "%s", p[counter].OffscreenWords);
+            counter++;
+        }   
+        fclose(fptr);
+    }else if (difficulty = 'm') {
         fptr = fopen("WordBankMedium.txt", "r");
-        break;
-    }else if (difficulty == 'h'){
-        fptr = fopen("WorkBankHard.txt", "r");
-        break;
-    }else{
-        printf("Please type one of the 3 letters corresponding to the difficulty\n");
-    }
+                 
+        while (fscanf(fptr, "%s", game[counter].OffscreenWords) != EOF){
+            fscanf(fptr, "%s", p[counter].OffscreenWords);
+            counter++;
+        }
+        fclose(fptr);
+    }else if (difficulty = 'h') {
+        fptr = fopen("WordBankHard.txt", "r");
+                 
+        while (fscanf(fptr, "%s", game[counter].OffscreenWords) != EOF){
+            fscanf(fptr, "%s", p[counter].OffscreenWords);
+            counter++;
+        }
+        fclose(fptr);
     }
 }
+
 void printDeathScreen() {
   
 }
