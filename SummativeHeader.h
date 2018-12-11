b2f962f623d5b627cb1a8261dbd0804bfa8289d9
LoveUtoo <45604857+LoveUtoo@users.noreply.github.com>
2018-12-11 14:01:34 -0500

    Update SummativeHeader.h

--- SummativeHeader.h
+++ SummativeHeader.h
@@ -1,5 +1,6 @@
 const int SCREEN_W = 900;       // screen width
 const int SCREEN_H = 480;       // screen height
+const int MID_SCREEN = (SCREEN_W + SCREEN_H) / 2;
 
 #define TEXTCOLOR al_map_rgb(255,255,255)
 #define BGCOLOR al_map_rgb(0,0,0)
