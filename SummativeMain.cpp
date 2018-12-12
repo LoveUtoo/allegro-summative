92af839d04b057b97ee0be0d389f63ed5bb572aa
LoveUtoo <45604857+LoveUtoo@users.noreply.github.com>
2018-12-12 13:33:05 -0500

    Update SummativeMain.cpp

--- SummativeMain.cpp
+++ SummativeMain.cpp
@@ -15,7 +15,8 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>                   // For allegro, must be in compiler search path.
-#include <allegro5/allegro_native_dialog.h> 		// for message box
+#include <allegro5/allegro_native_dialog.h>		// for message box
+#include <allegro5/allegro_image.h>
 #include "SummativeHeader.h"
 
 int main(int argc, char *argv[]) {
@@ -23,31 +24,33 @@ int main(int argc, char *argv[]) {
 	char difficulty;
     Words game[100];
     Stats player;
-	
+
    	//initialize Allegro
     initializeAllegro();
-    
+
     //declare and initialize display and font, and check they have been setup properly
     ALLEGRO_DISPLAY *display = al_create_display(SCREEN_W, SCREEN_H);
     ALLEGRO_FONT *font = al_load_ttf_font("Space Age.ttf", 36, 0);
     checkSetup(display, font);
 
+    ALLEGRO_MOUSE_STATE mouseState;
+
     //set window title
 	al_set_window_title(display, "Space-Word Blaster");
-	
+
 	//prints title screen
-	printTitleScreen();
-	
+	printTitleScreen(font, display, mouseState);
+
 	//starts the game
-	startGame();
-	
+	//startGame();
+
 	//ends the game
-	printDeathScreen();
-	
+	//printDeathScreen();
+
 	return 0;
-  
-  
-  
+
+
+
 }
 
 
