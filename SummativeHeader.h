dd2ba8c47b25bf56730dcaa7cdbcf1edcb5d0696
LoveUtoo <45604857+LoveUtoo@users.noreply.github.com>
2018-12-18 13:35:23 -0500

    Update SummativeHeader.h

--- SummativeHeader.h
+++ SummativeHeader.h
@@ -4,6 +4,8 @@ const int MID_SCREEN = (SCREEN_W) / 2;
 
 #define TEXTCOLOR al_map_rgb(255,255,255)
 #define BGCOLOR al_map_rgb(0,0,0)
+#define WHITE al_map_rgb(255,255,255)
+
 // Initializing more variables
 int lives = 3; 
 // The string that the user has typed in the box. The print words function also goes here. 
@@ -54,3 +56,4 @@ void startGame(Words game[]);
 int wordPerMin(int a, int b);
 int scoreDeterminer(char a[30]);
 char determineDifficulty();
+int mouseClick(imgData a, ALLEGRO_MOUSE_STATE mouseState);
