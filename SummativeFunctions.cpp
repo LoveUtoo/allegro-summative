d74a14e0586434dbad0a311e4bd66eadd529410b
LoveUtoo <45604857+LoveUtoo@users.noreply.github.com>
2018-12-18 13:34:36 -0500

    Update SummativeFunctions.cpp

--- SummativeFunctions.cpp
+++ SummativeFunctions.cpp
@@ -5,18 +5,28 @@
 #include <allegro5/allegro_native_dialog.h> 		// for message box
 #include "SummativeHeader.h"
 
-char determineDifficulty() {
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>               // For allegro, must be in compiler search path.
+#include <allegro5/allegro_native_dialog.h> 		// for message box
+#include "SummativeHeader.h"
 
-}
-
+//functions that displays title screen
 void printTitleScreen(ALLEGRO_FONT *font, ALLEGRO_DISPLAY *display, ALLEGRO_MOUSE_STATE mouseState) {
     ALLEGRO_BITMAP *startButton = al_load_bitmap("Waluigi.png");
-	
+
 	imgData StartButton;
 	StartButton.left = 338 ;
 	StartButton.right = 548 ;
 	StartButton.top = 64 ;
 	StartButton.bot = 253 ;
+
+	imgData QuitButton;
+	QuitButton.left = 400 ;
+	QuitButton.right = 500 ;
+	QuitButton.top = 300 ;
+	QuitButton.bot = 325 ;
 
     if (!startButton) {
     	al_show_native_message_box(display, "Error", "Error", "Failed to load start button image.",
@@ -27,6 +37,7 @@ void printTitleScreen(ALLEGRO_FONT *font, ALLEGRO_DISPLAY *display, ALLEGRO_MOUS
     al_draw_text(font, TEXTCOLOR, MID_SCREEN, 5, ALLEGRO_ALIGN_CENTRE, "SPACE-WORD BLASTER EXTREME");
     al_draw_bitmap(startButton, 275, 25, 0);
     al_draw_text(font, TEXTCOLOR, MID_SCREEN, 265, ALLEGRO_ALIGN_CENTRE, "LEADERBOARDS");
+    al_draw_text(font, TEXTCOLOR, MID_SCREEN, 295, ALLEGRO_ALIGN_CENTRE, "QUIT");
 
     al_flip_display();
 
@@ -34,46 +45,54 @@ void printTitleScreen(ALLEGRO_FONT *font, ALLEGRO_DISPLAY *display, ALLEGRO_MOUS
 
         al_get_mouse_state(&mouseState);
         printf("%d %d %0.2f\n", mouseState.x, mouseState.y, mouseState.pressure);
-	    
-	if (mouseState.x > StartButton.left && mouseState.x < StartButton.right && mouseState.y > StartButton.top && mouseState.y < StartButton.bot && mouseState.pressure == 1.0) {
-            printf("test");
-        }
-	    
-        al_rest(0.01);
+
+	if (mouseClick(StartButton, mouseState) == 1) {
+        al_clear_to_color(BGCOLOR);
+        al_flip_display();
+        startGame();
+    }else if (mouseClick(QuitButton, mouseState) == 1){
+        al_clear_to_color(BGCOLOR);
+        al_flip_display();
     }
+
+    al_rest(0.01);
+    }
+
 }
 
-void startGame(Words game[]) {
+//function that starts the game
+void startGame() {
+    Words game;
     int counter = 0;
     FILE *fptr;
     printf("What difficulty do you want to play? Easy, Medium, Hard (e/m/h)\n");
     char difficulty = determineDifficulty();
-    if (difficulty = 'e'){
+    if (difficulty == 'e'){
         fptr = fopen("WordBankEasy.txt", "r");
-
-        while (fscanf(fptr, "%s", game[counter].OffscreenWords) != EOF){
-            fscanf(fptr, "%s", game[counter].OffscreenWords);
-            counter++;
-        }
-        fclose(fptr);
-    }else if (difficulty = 'm') {
+    }else if (difficulty == 'm') {
         fptr = fopen("WordBankMedium.txt", "r");
-
-        while (fscanf(fptr, "%s", game[counter].OffscreenWords) != EOF){
-            fscanf(fptr, "%s", game[counter].OffscreenWords);
-            counter++;
-        }
-        fclose(fptr);
-    }else if (difficulty = 'h') {
+    }else if (difficulty == 'h') {
         fptr = fopen("WordBankHard.txt", "r");
-
-        while (fscanf(fptr, "%s", game[counter].OffscreenWords) != EOF){
-            fscanf(fptr, "%s", game[counter].OffscreenWords);
-            counter++;
-        }
-        fclose(fptr);
     }
+
+    while (fscanf(fptr, "%s", game.OffscreenWords[counter]) != EOF){
+        fscanf(fptr, "%s", game.OffscreenWords[counter]);
+        counter++;
+    }
+        fclose(fptr);
+
+    ALLEGRO_BITMAP *ship = al_load_bitmap("shipPlaceholder.png");
+    ALLEGRO_BITMAP *gun = al_load_bitmap("cannonPlaceholder.png");
+
+    al_convert_mask_to_alpha(ship, WHITE);
+    al_convert_mask_to_alpha(gun, WHITE);
+    al_draw_bitmap(ship, 250, 25, 0);
+    al_draw_bitmap(gun, 300, 30, 0);
+
+    al_flip_display();
+    al_rest(20);
 }
+
 void generateWord(int &wordbank[30][35]){
     for(int i = 0; i < 31; i++){
             // while this is not the end of the file
@@ -109,3 +128,16 @@ void printDeathScreen() {
   fgets()    
 }
 
+	//function that determines difficulty
+char determineDifficulty() {
+    char a = 'e';
+    return a;
+}
+
+int mouseClick(imgData a, ALLEGRO_MOUSE_STATE mouseState){
+    if (mouseState.x > a.left && mouseState.x < a.right && mouseState.y > a.top && mouseState.y < a.bot && mouseState.pressure == 1.0) {
+        return 1;
+    }else if (mouseState.x > a.left && mouseState.x < a.right && mouseState.y > a.top && mouseState.y < a.bot && mouseState.pressure != 1.0) {
+        return 0;
+    }
+}
