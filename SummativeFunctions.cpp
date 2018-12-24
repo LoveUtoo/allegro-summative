42318a3a0391ac94e28ac1bc4718e987e2025281
allancao1 <45244917+allancao1@users.noreply.github.com>
2018-12-23 21:19:38 -0500

    Update SummativeFunctions.cpp

--- SummativeFunctions.cpp
+++ SummativeFunctions.cpp
@@ -3,7 +3,7 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>               // For allegro, must be in compiler search path.
 #include <allegro5/allegro_native_dialog.h> 		// for message box
-#include "SummativeHeader.h"
+#include "SummativeHeader.h"// the header
 
 #include <stdio.h>
 #include <allegro5/allegro.h>
