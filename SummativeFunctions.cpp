9104cd6dcc3ed22bd2094f76e780c6cb2a248de0
allancao1 <45244917+allancao1@users.noreply.github.com>
2019-01-09 14:24:32 -0500

    Update SummativeFunctions.cpp

--- SummativeFunctions.cpp
+++ SummativeFunctions.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>               // For allegro, must be in compiler search path.
